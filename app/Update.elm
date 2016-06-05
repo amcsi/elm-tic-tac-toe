b63d98d7af4117b6027f614857313a4381147f1c
Attila Szeremi <attila@szeremi.org>
2016-06-05 19:44:27 +0100

    Fix premature player change bug

--- app/Update.elm
+++ app/Update.elm
@@ -4,30 +4,49 @@ import Action exposing (Action(..))
 import Model exposing (Model)
 import List exposing (indexedMap)
 import Types exposing (Piece(..), Player(..))
+import Zone exposing (Zone)
 
 update : Action -> Model -> (Model, Cmd Action)
 update action model =
   case action of
     -- Place a piece in the given index; assumed the move is legal
     PlacePieceInIndex flatIndex ->
-      ( { model |
-          board =
-            let
-                mapper : Int -> Maybe Piece -> Maybe Piece
-                mapper index currentPiece =
-                  if index == flatIndex
-                  then if currentPiece == Nothing then Just model.pieceOfPlayer else currentPiece
-                  else currentPiece
-            in indexedMap mapper model.board
-        -- Flip the turn players
-        , turnPlayer =
-          if model.turnPlayer == Player1
-          then Player2
-          else Player1
-        -- Flip the current player's piece
-        , pieceOfPlayer =
-          if model.pieceOfPlayer == X then O else X
-        }
-      , Cmd.none
-      )
+      let
+        zoneToPlacePlayerPiece : Int -> List (Maybe Piece) -> Maybe Zone -> Maybe Zone
+        zoneToPlacePlayerPiece index pieces acc =
+          case pieces of
+            [] -> acc
+            piece :: pieces ->
+              let recursiveResult = zoneToPlacePlayerPiece (index + 1) pieces acc
+              in
+                if index == flatIndex
+                then case piece of
+                  Nothing -> Just (Zone.fromFlat index)
+                  _ -> recursiveResult
+                else recursiveResult
+        zone = zoneToPlacePlayerPiece 0 model.board Nothing
+      in case zone of
+        Nothing -> (model, Cmd.none)
+        Just zone ->
+          let
+            index = Zone.toFlat <| zone
+          in
+            ( { model |
+              board =
+                let
+                  mapper : Int -> Maybe Piece -> Maybe Piece
+                  mapper index' currentPiece =
+                    if index == index' then Just model.pieceOfPlayer else currentPiece
+                in indexedMap mapper model.board
+              -- Flip the turn players
+              , turnPlayer =
+                if model.turnPlayer == Player1
+                then Player2
+                else Player1
+              -- Flip the current player's piece
+              , pieceOfPlayer =
+                if model.pieceOfPlayer == X then O else X
+              }
+            , Cmd.none
+            )
     _ -> (model, Cmd.none)
